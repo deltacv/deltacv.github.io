ad5915682790a060e7ab1c8a40191c9f1d684b3a
Sebastian Erives <serivesmejia@deltacv.org>
2025-03-12 19:40:27 -0600

    add mc

--- src/App.svelte
+++ src/App.svelte
@@ -3,43 +3,70 @@
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import Project from './projects/Project.svelte';
-  
+
 	let ready = false;
-	onMount(() => setTimeout(() => ready = true, 500));
-  </script>
-  
-  {#if ready}
-  <div class="container">
+	let imagesLoaded = 0;
+	const imageSources = [
+		"/eocvsim.png",
+		"/papervision.gif",
+		"/minecraft.gif"
+	];
+
+	function checkImagesLoaded() {
+		imagesLoaded++;
+		if (imagesLoaded === imageSources.length) {
+			ready = true;
+		}
+	}
+
+	onMount(() => {
+		imageSources.forEach(src => {
+			const img = new Image();
+			img.src = src;
+			img.onload = checkImagesLoaded;
+			img.onerror = checkImagesLoaded; // In case of an error, still count it as loaded
+		});
+	});
+</script>
+
+{#if ready}
+<div class="container">
 	<div class="header">
-	  <div class="logo">
-		<AnimatedLogoText/>
-	  </div>
+		<div class="logo">
+			<AnimatedLogoText/>
+		</div>
 	</div>
-  
+
 	<div class="content" in:fade={{duration: 1000}}>
-	  <Project 
-		title="EOCV-Sim" 
-		description="Develop, test, and tune your EasyOpenCV pipelines directly on your computer with a simple GUI!"
-		gifSrc="/eocvsim.png"
-		href="https://docs.deltacv.org/eocv-sim"
-	  />
-  
-	  <Project 
-		title="PaperVision" 
-		description="Create your custom OpenCV algorithms using a user-friendly node editor interface inspired by Blender and Unreal Engine blueprints."
-		gifSrc="/papervision.gif"
-		href="https://github.com/deltacv/PaperVision"
-	  />
+		<Project 
+			title="EOCV-Sim" 
+			description="Develop, test, and tune your EasyOpenCV pipelines directly on your computer with a simple GUI!"
+			gifSrc="/eocvsim.png"
+			href="https://docs.deltacv.org/eocv-sim"
+		/>
+
+		<Project 
+			title="PaperVision" 
+			description="Create your custom OpenCV algorithms using a user-friendly node editor interface inspired by Blender and Unreal Engine blueprints."
+			gifSrc="/papervision.gif"
+			href="https://github.com/deltacv/PaperVision"
+		/>
+
+		<Project 
+			title="Minecraft Server" 
+			description="By FIRST alumni, for FIRST students. Join our Minecraft server to hang out and play unique modes!"	
+			gifSrc="/minecraft.gif"
+			href="/minecraft"
+		/>
 	</div>
-  
-	<!-- Footer Section -->
+
 	<footer class="footer">
-	  <div class="contact-info">
-		<p>Contact us at: <a href="mailto:dev@deltacv.org">dev@deltacv.org</a>. Follow us on GitHub: <a href="https://github.com/deltacv" target="_blank">deltacv</a></p>
-	  </div>
+		<div class="contact-info">
+			<p>Contact us at: <a href="mailto:dev@deltacv.org">dev@deltacv.org</a>. Follow us on GitHub: <a href="https://github.com/deltacv" target="_blank">deltacv</a></p>
+		</div>
 	</footer>
-  </div>
-  {/if}
+</div>
+{/if}
   
   <style>
 	:global(body) {
