d1af8a306aff6d6b19f240ed525d28df12b4b01a
Sebastian Erives <serivesmejia@deltacv.org>
2026-09-01 14:40:56 -0600

    Add canonical http header in [username]

--- src/routes/[username]/+page.ts
+++ src/routes/[username]/+page.ts
@@ -13,9 +13,12 @@ const authors = Object.entries(authorModules).map(([path, mod]: [string, any]) =
     return { ...mod.author, href: `/people/${folder}` };
 });
 
-export const load: PageLoad = ({ params }) => {
+export const load: PageLoad = ({ params, setHeaders }) => {
     const match = authors.find(a => a.shortSlash === params.username);
     if (match) {
+        setHeaders({
+            'Link': `<https://deltacv.org${match.href}>; rel="canonical"`
+        });
         redirect(301, match.href);
     }
     error(404, 'Not Found');
